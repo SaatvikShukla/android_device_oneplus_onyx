44ec9231c5a71c7de99594970eb0a6ab338a66f1
Saatvik Shukla <me@saatvikshukla.com>
2016-08-29 21:45:07 +0530

    Sepolicy: Disable call for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,7 +159,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_NO_RPC := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+# include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     $(PLATFORM_PATH)/sepolicy
